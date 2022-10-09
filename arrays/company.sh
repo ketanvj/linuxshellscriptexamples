281639ef6a2746c9084bba6a79cad2381faf64ce
Ketan J <ketanvj@yahoo.com>
2022-10-09 06:17:29 +0000

    added array examples in array folder

---
+++ arrays/company.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+company=("Infosys" "TCS" "IBM") # space and NOT comma delimited	
+echo ${company[0]}
+echo ${company[*]} or echo ${company[@]}
---
+++ arrays/num.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+NUM=(1 3 6 2 6 2 5)
+echo $NUM # only the first value
+echo ${NUM[3]} # 2
+
